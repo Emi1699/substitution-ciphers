9618f512c0ca2c8c42b4f3dac6e76c90160d80c1
Emi1699 <emisv_16@yahoo.ro>
2019-10-15 23:07:28 +0100

    it is possible to decrypt messages now

--- Homework2.hs
+++ Homework2.hs
@@ -13,30 +13,48 @@ module Homework2 where
 	validateCipher :: Cipher -> Bool
 	validateCipher cipher = length cipher == 26 && charsUniq cipher
 
+
 	charsUniq :: String -> Bool
 	charsUniq cipher
 		| length cipher == 1 = True -- the character must be unique is it's the only one in the list
 		| elem (head cipher) (tail cipher) = False -- if the character's present in multiple places in the list, then it's not unique
 		| otherwise = charsUniq (tail cipher) -- check recursively if the remaining elements are unique
 
+
 	encode :: Cipher -> Int -> Char -> Char
 	encode cipher offset character
-		| (alphaPos character - offset) `mod` 26 == (alphaPos (currentChr cipher)) `mod` 26 = head cipher 
+		| (alphaPos character - offset) `mod` 26 == (alphaPos (currentAlphabetChr cipher)) `mod` 26 = head cipher 
 		| otherwise = encode (tail cipher) offset character
-
-	{- 
-	 because the mapping is static, we're always moving through both the alphabet and the cipher at the same rate
-	 so, 'currentChr cipher' will return the letter of the alphabet that corresponds to the character we're currently at in the cipher
-	 I felt this clarification might be needed in order to not confuse future me when looking over the code
-	-}
-	currentChr :: Cipher -> Char
-	currentChr cipher = chr (91 - length cipher)
 
 	encodeMessage :: Cipher -> Int -> String -> String
 	encodeMessage cipher offset message
 		| length message > 0 = encode cipher offset (head message) : encodeMessage cipher offset (tail message)
 		| otherwise = [] -- doesn't work without this guard???
 
+
+	reverseEncode :: Cipher -> Int -> Char -> Char
+	reverseEncode cipher offset encChar
+		| encChar == head cipher = chr (((alphaPos (currentAlphabetChr cipher) + offset) `mod` 26) + ord 'A')
+		| otherwise = reverseEncode (tail cipher) offset encChar
+
+	reverseEncodeMessage :: Cipher -> Int -> String -> String
+	reverseEncodeMessage cipher offset encMessage
+		| length encMessage > 0 = reverseEncode cipher offset (head encMessage) : reverseEncodeMessage cipher offset (tail encMessage)
+		| otherwise = []
+
+	letterStats :: String -> [(Char, Int)]
+	letterStats message
+		| 
+		| oherwise = []
+
+
+	{- 
+	 we're always moving through both the alphabet and the cipher at the same rate
+	 so, 'currentChr cipher' will return the letter of the alphabet that corresponds to the character we're currently at in the cipher
+	 I felt this clarification might be needed in order to not confuse future me when looking over the code
+	-}
+	currentAlphabetChr :: String -> Char
+	currentAlphabetChr cipher = chr (91 - length cipher)
 
 
 
